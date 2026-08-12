2e2e843961ea87ff9099fc42abaa73fc7c1d27e3
Pedro <pedrohferreira005@gmail.com>
2026-08-12 11:38:52 -0300

    atualização das rotas para conter posts de usuários, criação de templates html para implementar a página inicial simples.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username' : 'Pedro'}
+    posts = [
+        {
+            "author":{"username" : "João"},
+         "body" : "Grande dia em João Pessoa!"
+         },
+         {
+             "author":{"username":"Maria"},
+             "body":"O filme dos Vingadores foi massa"
+         }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
